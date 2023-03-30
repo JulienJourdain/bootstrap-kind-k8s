cfacf459e40adf7b76f688e67571b773b1679db7
Yann Jacquot <yannj@theodo.fr>
2023-03-30 16:03:13 +0200

    Add API call between MS1 and MS2

--- docker/app-1/src/main/java/com/example/ms1/HealthcheckController.java
+++ docker/app-1/src/main/java/com/example/ms1/HealthcheckController.java
@@ -3,8 +3,10 @@ package com.example.ms1;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @RestController
@@ -12,6 +14,9 @@ public class HealthcheckController {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
+
+    @Value("${endpoint.ms2-url}")
+    private String ms2Url;
 
 	@GetMapping("/health-check")
 	public String index() {
@@ -23,5 +28,13 @@ public class HealthcheckController {
         UUID uuid = (UUID) jdbcTemplate.queryForObject("SELECT UUID()", UUID.class);
 
         return uuid.toString();
+    }
+
+    @GetMapping("/query/ms2")
+    public String ms2Query() {
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(ms2Url+"/query", String.class);
+
+        return result;
     }
 }
